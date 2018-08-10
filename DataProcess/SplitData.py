c373b941d769ffd9dcf4cd805d6577150d73a9bf
zhilin gong <dashuzhilin@gmail.com>
2018-08-10 10:24:31 +0800

    Add files via upload

---
+++ DataProcess/SplitData.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import random
+
+class Split:
+
+    # split data for training and testing
+
+
+
+    def __init__(self):
+        self.origin_path = 'Data/{}'
+
+    def split(self, M, k, seed):
+
+        data = []
+        train = []
+        test = []
+
+        print('Generate train set and test set...')
+
+        # f = open(self.origin_path.format('ratings.csv'), 'r')
+        # f.readline()
+        f = open(self.origin_path.format('u.data'), 'r')
+        line = f.readline()
+        while line:
+            # line_split = line.split(',')
+            line_split = line.split('\t')
+            data.append((int(line_split[0]), int(line_split[1]), int(line_split[2])))
+            line = f.readline()
+        f.close()
+
+        random.seed(seed)
+        for user,item,rating in data:
+            if random.randint(0,M)==k:
+                test.append((user,item,rating))
+            else:
+                train.append((user,item,rating))
+
+        return train,test
