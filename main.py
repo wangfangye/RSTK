826d86eaaabff56813a1645c6da9578c7c5c26b4
wangfangye <786632563@qq.com>
2018-08-29 14:44:43 +0800

    Feature/bpr (#7)
    * update BPR
    
    * Update FM.py
    
    解决冲突
    
    * Update FM_workflow.py
    
    手滑误删，修复一下。

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
 from DataProcess.TypeTransfer import Transfer
 from Workflow.LFM_workflow import run as lfm
+
+from Workflow.BPR_workflow import run as bpr
 from Workflow.FM_workflow import run as fm
 from Workflow.KNN_workflow import run as knn
+
 
 
 def choose():
@@ -11,6 +14,8 @@ def choose():
         lfm()
     elif arg == 'fm':
         fm()
+    elif arg == 'bpr':
+        bpr()
     elif arg == 'knn':
         knn()    
     sys.exit()
