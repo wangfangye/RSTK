921b9701629d58dbf496bb47d6e1b3f67a1fa791
dashuzhilin <413421391@qq.com>
2018-08-29 11:07:19 +0800

    Add UserKNN and ItemKNN

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 from DataProcess.TypeTransfer import Transfer
 from Workflow.LFM_workflow import run as lfm
+from Workflow.KNN_workflow import run as knn
 
 def choose():
     arg = sys.argv[1]
     if arg == 'lfm':
         lfm()
+    elif arg == 'knn':
+        knn()
     sys.exit()
 
 if __name__ == '__main__':
